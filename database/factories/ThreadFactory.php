00d2ffb5e074d35ae94d94ecde08e77ff9b172b0
Aung Htet Paing <apaing894@gmail.com>
2020-11-27 10:38:57 +0630

    Database seeding & setup.

---
+++ database/factories/ThreadFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Thread;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ThreadFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Thread::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => User::factory(),
+            'title' => $this->faker->title,
+            'body' => $this->faker->paragraph()
+        ];
+    }
+}
